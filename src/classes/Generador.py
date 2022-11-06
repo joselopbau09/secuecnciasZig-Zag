306585b851ce164248f754a13abd8e08fabe4073
Jose Luis Bautista <joseluis27@ciencias.unam.mx>
2022-11-05 19:25:55 -0600

    Clase generadora de la secuencia

--- src/classes/Generador.py
+++ src/classes/Generador.py
@@ -1,8 +1,30 @@
 
 class Generador:
     def __init__(self,longitudSecuencia, numZigZag):
-        self.longitudSecuencia = longitudSecuencia
-        self.numZigZag = numZigZag
         self.secuencia = []
+        self.generaSecuencia(longitudSecuencia, numZigZag)
     
-        
+    def generaSecuencia(self,longitudSecuencia, numZigZag):
+        mitadSecuencia = longitudSecuencia // 2
+        mitadSecuenciaAvanza = mitadSecuencia + 1 
+        avanzaRetrocede = True
+        elemtosAgergados = 0 
+
+        for i in range(0, longitudSecuencia):
+            if avanzaRetrocede:
+                self.secuencia.append(mitadSecuenciaAvanza)
+                mitadSecuenciaAvanza += 1
+                elemtosAgergados += 1
+                if(elemtosAgergados == numZigZag):
+                    avanzaRetrocede = False
+                    elemtosAgergados = 0
+            else:
+                self.secuencia.append(mitadSecuencia)
+                mitadSecuencia -= 1 
+                elemtosAgergados += 1
+                if(elemtosAgergados == numZigZag):
+                    avanzaRetrocede = True
+                    elemtosAgergados = 0            
+
+    def getSecuencia(self):
+        return self.secuencia
